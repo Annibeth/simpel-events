9a7e35977d61fee9bad9136441bc7e085b60c50c
Admin <Admin>
2016-11-11 09:58:12 +0100

    add all

---
+++ simpel-events/Observer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simple_event
+{
+    class Observer
+    {
+        public void HandleEvent(object sender, EventArgs args)
+        {
+            Console.WriteLine("Something happened to " + sender);
+        }
+    }
+}
---
+++ simpel-events/Test.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simple_event
+{
+    class Test // det samme som program.cs
+    {
+        static void Main()
+        {
+            Observerable observable = new Observerable();
+            Observer observer = new Observer();
+            observable.SomethingHappened += observer.HandleEvent;
+
+            observable.DoSomething();
+
+        }
+    }
+}
